5ce6324a625f7a00c9c997a787c7a5e3ebdc7721
unknown <juan.bernal05@usa.edu.co>
2024-10-19 15:25:09 -0500

    Replace and reupload project

---
+++ spring-microservices-backend/microservice-ordenes/src/main/java/microservice/service/OrdenServiceImpl.java
@@ -0,0 +1,25 @@
+package microservice.service;
+
+import microservice.entities.Orden;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import microservice.persistence.ordenRepository;
+
+import java.util.List;
+
+@Service
+public class OrdenServiceImpl implements OrdenService {
+
+    @Autowired
+    private OrdenRepository ordenRepository;
+
+    @Override
+    public List<Orden> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Orden findById(Long id) {
+        return null;
+    }
+}
