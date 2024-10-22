718356263465e68f9b2e8f23bce2dc07cbc938ef
Haunes <juantienecell@gmail.com>
2024-10-21 22:37:00 -0500

    microservicio de inventario agregado

---
+++ spring-microservices-backend/microservice-inventario/microservice-inventario/src/test/java/com/microservices/gateway/microservice_inventario/MicroserviceInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.gateway.microservice_inventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
