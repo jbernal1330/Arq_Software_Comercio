18407ac9a8c57c04a2c17380db4bcd15db50c383
Sofia Velandia Sierra <84143331+so-fivs@users.noreply.github.com>
2024-11-23 20:30:01 -0500

    subir conexiones rabbir¿tmq
    (cherry picked from commit f5c09b5c725ba752d6bc58b896d317aa56df288d)

---
+++ springboot-rabbitmq-services/src/main/java/net/javaguides/springboot/consumer/RabbitMQJsonConsumer.java
@@ -0,0 +1,16 @@
+package net.javaguides.springboot.consumer;
+
+import net.javaguides.springboot.dto.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQJsonConsumer {
+    private static final Logger LOGGER= LoggerFactory.getLogger(RabbitMQJsonConsumer.class);
+    @RabbitListener(queues = "${rabbitmq.queue.json.name}")
+    public void Jsonconsumer(User user){
+        LOGGER.info("Recibido mensaje -> {}", user);
+    }
+}
